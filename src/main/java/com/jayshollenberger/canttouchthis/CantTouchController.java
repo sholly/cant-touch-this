b9be389002559a03a46d61c852ef3fdc26d8e0e6
Jay Shollenberger <jshollen@redhat.com>
2021-06-20 17:22:18 -0600

    v0.0.3

--- src/main/java/com/jayshollenberger/canttouchthis/CantTouchController.java
+++ src/main/java/com/jayshollenberger/canttouchthis/CantTouchController.java
@@ -13,17 +13,12 @@ public class CantTouchController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping("/")
     public String root() {
-        return "can't touch this v0.0.2";
+        return "can't touch this v0.0.3";
     }
 
     @GetMapping("/cant")
     public String cantTouch() {
-        return "v0.0.2";
-    }
-
-    @GetMapping("/touch")
-    public String noTouching() {
-        return "no touching";
+        return "v0.0.3";
     }
 
     @GetMapping("/showenv")
